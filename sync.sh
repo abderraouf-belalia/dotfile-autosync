8ade0be46f117dbc01e0a916523c4e75f370627a
Abderraouf Belalia <abderraoufbelalia@gmail.com>
2023-10-02 17:45:54 +0100

    New backup 2023-10-02 17:45:54

--- sync.sh
+++ sync.sh
@@ -19,42 +19,10 @@ git fetch
 
 # Syncing dotfiles
 ## NVIM 
-bisync "nvim" dotfiles/nvim
+bisync "nvim" dotfiles/nvim/
 
 ## ZSH
-bisync "zsh" dotfiles/zsh
+bisync "zsh" dotfiles/zsh/
 
 # Backup local changes
 gitlocalcheck
-
-
-# FUNCTIONS
-# Check git status
-function gitlocalcheck {
-	GIT_STATUS="$(git status | grep -i "modified")"
-	
-	# If there is a new change
-	if [[ $GIT_STATUS == *"modified"* ]]; then
-	  echo "[*] Pushing to origin ..."
-	  gitpush
-	fi
-	
-}
-
-# Push to Github
-function gitpush {
-	git add -u;
-	git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
-	git push origin master
-}
-
-# Sync folders back and forth
-function bisync {
-	local PROGRAM=$1
-	local BACKUP_PATH=$2
-	grep "$PROGRAM" dotfile.paths | awk 'BEGIN{FS=":"}{print $2}' | while read -r path; 
-	do 
-		rsync -avu $BACKUP_PATH $path
-		rsync -avu $path $BACKUP_PATH 
-	done
-}

5f2eedacbec0da1f1840a581a12305b2da4fc6fd
Abderraouf Belalia <abderraoufbelalia@gmail.com>
2023-10-05 14:29:53 -0700

    New backup 2023-10-05 14:29:53

--- dotfiles/nvim/lua/core/plugins.lua
+++ dotfiles/nvim/lua/core/plugins.lua
@@ -30,7 +30,15 @@ return require('packer').startup(function(use)
     })
 
   -- editor
-  use 'tpope/vim-surround'
+  use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
   use 'nvim-treesitter/nvim-treesitter'
     -- appearance
     use 'overcache/NeoSolarized'

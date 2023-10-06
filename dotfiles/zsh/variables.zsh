b7a7c4de2bed3c2854d9bbb3e7238bd433736758
Abderraouf Belalia <abderraoufbelalia@gmail.com>
2023-10-05 22:30:16 -0700

    New backup 2023-10-05 22:30:16

--- dotfiles/zsh/variables.zsh
+++ dotfiles/zsh/variables.zsh
@@ -1,1 +1,2 @@
-export OPENAI_API_KEY=$(cat $HOME/.secrets | grep "openai" | awk 'BEING{FS=":"}{print $2}')
+export OPENAI_API_KEY="$(cat $HOME/.secrets | grep "openai" | awk 'BEING{FS=":"}{print $2}')"
+echo $OPENAI_API_KEY

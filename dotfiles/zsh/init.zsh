b3ae1d526e65c5678a3aec63a74c9dfcf5c22435
Abderraouf Belalia <abderraoufbelalia@gmail.com>
2023-10-05 22:07:35 -0700

    New backup 2023-10-05 22:07:35

--- dotfiles/zsh/init.zsh
+++ dotfiles/zsh/init.zsh
@@ -1,6 +1,7 @@
-ZSH_CONFIG=".config/zsh"
+ZSH_CONFIG=$HOME"/.config/zsh"
 
+source $ZSH_CONFIG"/variables.zsh"
 source $ZSH_CONFIG"/options.zsh"
 source $ZSH_CONFIG"/functions.zsh"
-source $ZSH_CONFIG"/appearance.zsh"
 source $ZSH_CONFIG"/aliases.zsh"
+source $ZSH_CONFIG"/appearance.zsh"
---
+++ dotfiles/zsh/variables.zsh
@@ -0,0 +1,2 @@
+OPENAI_API_KEY=$(cat $HOME/.secrets | grep "openai" | awk 'BEING{FS=":"}{print $2}')
+

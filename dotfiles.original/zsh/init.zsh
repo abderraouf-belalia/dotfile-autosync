badd4067eaa2db393837be7d5ccdd8c9c1af21e2
Abderraouf Belalia <abderraoufbelalia@gmail.com>
2023-10-21 03:13:08 -0700

    Rotation complete.

--- dotfiles.original/zsh/init.zsh
+++
@@ -1,8 +0,0 @@
-ZSH_CONFIG=$HOME"/.config/zsh"
-
-source $ZSH_CONFIG"/variables.zsh"
-source $ZSH_CONFIG"/options.zsh"
-source $ZSH_CONFIG"/functions.zsh"
-source $ZSH_CONFIG"/aliases.zsh"
-source $ZSH_CONFIG"/appearance.zsh"
-source $ZSH_CONFIG"/variables.zsh"
